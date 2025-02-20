81205818bd3bb702bed873d30a1d7c3bb5265c8b
CauZy-Goes <cauafariasdev@gmail.com>
2025-02-20 15:41:57 -0300

    feat: add findByUsuarioSolicitante_IdUsuario

--- src/main/java/ucsal/cauzy/domain/repository/SolicitacoesRepository.java
+++ src/main/java/ucsal/cauzy/domain/repository/SolicitacoesRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ucsal.cauzy.domain.entity.Solicitacoes;
 
+import java.util.List;
+
 public interface SolicitacoesRepository extends JpaRepository<Solicitacoes, Integer> {
+
+    // Buscar todas as solicitações feitas por um professor específico
+     List<Solicitacoes> findByUsuarioSolicitante_IdUsuario(Integer idUsuarioSolicitante);
 }
 
