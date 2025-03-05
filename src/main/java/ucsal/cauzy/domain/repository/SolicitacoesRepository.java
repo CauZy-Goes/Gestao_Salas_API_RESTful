73de69a8db7e6edfea3983a6074802ecc75aefcf
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-05 12:07:38 -0300

    feat: add method deleteByEspacoFisicoId on SolicitacoesRepository

--- src/main/java/ucsal/cauzy/domain/repository/SolicitacoesRepository.java
+++ src/main/java/ucsal/cauzy/domain/repository/SolicitacoesRepository.java
@@ -2,6 +2,9 @@ package ucsal.cauzy.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ucsal.cauzy.domain.entity.Solicitacoes;
 
 import java.util.List;
@@ -10,5 +13,9 @@ public interface SolicitacoesRepository extends JpaRepository<Solicitacoes, Inte
 
     // Buscar todas as solicitações feitas por um professor específico
      List<Solicitacoes> findByUsuarioSolicitante_IdUsuario(Integer idUsuarioSolicitante);
+
+    @Modifying
+    @Query("DELETE FROM Solicitacoes s WHERE s.espacoFisico.idEspacoFisico = :idEspaco")
+    void deleteByEspacoFisicoId(@Param("idEspaco") Integer idEspaco);
 }
 
