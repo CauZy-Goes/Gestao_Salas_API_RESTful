ce0d4458f29742828ad962c9c2aada61dcbbf166
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-27 20:18:50 -0300

    feat: add lombok in entity cargo

--- src/main/java/ucsal/cauzy/domain/entity/Cargo.java
+++ src/main/java/ucsal/cauzy/domain/entity/Cargo.java
@@ -1,10 +1,17 @@
 package ucsal.cauzy.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 
 @Entity
 @Table(name = "cargo")
+@Getter
+@Setter
+@ToString
 public class Cargo implements Serializable {
 
     @Id
@@ -14,26 +21,26 @@ public class Cargo implements Serializable {
     @Column(name = "nome_cargo", nullable = false, length = 50)
     private String nomeCargo;
 
-	public Integer getIdCargo() {
-		return idCargo;
-	}
-
-	public void setIdCargo(Integer idCargo) {
-		this.idCargo = idCargo;
-	}
-
-	public String getNomeCargo() {
-		return nomeCargo;
-	}
-
-	public void setNomeCargo(String nomeCargo) {
-		this.nomeCargo = nomeCargo;
-	}
-
-	@Override
-	public String toString() {
-		return "Cargo [idcargo=" + idCargo + ", nomeCargo=" + nomeCargo + "]";
-	}
+//	public Integer getIdCargo() {
+//		return idCargo;
+//	}
+//
+//	public void setIdCargo(Integer idCargo) {
+//		this.idCargo = idCargo;
+//	}
+//
+//	public String getNomeCargo() {
+//		return nomeCargo;
+//	}
+//
+//	public void setNomeCargo(String nomeCargo) {
+//		this.nomeCargo = nomeCargo;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Cargo [idcargo=" + idCargo + ", nomeCargo=" + nomeCargo + "]";
+//	}
 	
 	
 }
