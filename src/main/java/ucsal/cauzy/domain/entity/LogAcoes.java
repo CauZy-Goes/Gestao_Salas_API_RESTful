e80c81124ad7bc4ef7adb96d1d949d8d38f0e19a
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-27 20:14:47 -0300

    feat: add lombok in entity LogAcoes

--- src/main/java/ucsal/cauzy/domain/entity/LogAcoes.java
+++ src/main/java/ucsal/cauzy/domain/entity/LogAcoes.java
@@ -1,11 +1,18 @@
 package ucsal.cauzy.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "log_acoes")
+@Getter
+@Setter
+@ToString
 public class LogAcoes implements Serializable {
 
     @Id
@@ -22,42 +29,42 @@ public class LogAcoes implements Serializable {
     @Column(name = "acao", nullable = false, length = 255)
     private String acao;
 
-	public Integer getIdLogAcoes() {
-		return idLogAcoes;
-	}
-
-	public void setIdLogAcoes(Integer idlogacoes) {
-		this.idLogAcoes = idlogacoes;
-	}
-
-	public LocalDateTime getDataHora() {
-		return dataHora;
-	}
-
-	public void setDataHora(LocalDateTime dataHora) {
-		this.dataHora = dataHora;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public String getAcao() {
-		return acao;
-	}
-
-	public void setAcao(String acao) {
-		this.acao = acao;
-	}
-
-	@Override
-	public String toString() {
-		return "LogAcoes [idlogacoes=" + idLogAcoes + ", dataHora=" + dataHora + ", usuario=" + usuario + ", acao="
-				+ acao + "]";
-	}
+//	public Integer getIdLogAcoes() {
+//		return idLogAcoes;
+//	}
+//
+//	public void setIdLogAcoes(Integer idlogacoes) {
+//		this.idLogAcoes = idlogacoes;
+//	}
+//
+//	public LocalDateTime getDataHora() {
+//		return dataHora;
+//	}
+//
+//	public void setDataHora(LocalDateTime dataHora) {
+//		this.dataHora = dataHora;
+//	}
+//
+//	public Usuario getUsuario() {
+//		return usuario;
+//	}
+//
+//	public void setUsuario(Usuario usuario) {
+//		this.usuario = usuario;
+//	}
+//
+//	public String getAcao() {
+//		return acao;
+//	}
+//
+//	public void setAcao(String acao) {
+//		this.acao = acao;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "LogAcoes [idlogacoes=" + idLogAcoes + ", dataHora=" + dataHora + ", usuario=" + usuario + ", acao="
+//				+ acao + "]";
+//	}
 }
 
