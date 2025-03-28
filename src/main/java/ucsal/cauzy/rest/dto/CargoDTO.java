ae660ab79293c7f5371702d26ca4c4b7655fa4d9
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-28 09:26:05 -0300

    feat: refactor cargo dto

--- src/main/java/ucsal/cauzy/rest/dto/CargoDTO.java
+++ src/main/java/ucsal/cauzy/rest/dto/CargoDTO.java
@@ -1,25 +1,15 @@
 package ucsal.cauzy.rest.dto;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public class CargoDTO {
-    private Integer idCargo;
-    private String nomeCargo;
-
-    // Getters e Setters
-    public Integer getIdCargo() {
-        return idCargo;
-    }
-
-    public void setIdCargo(Integer idCargo) {
-        this.idCargo = idCargo;
-    }
-
-    public String getNomeCargo() {
-        return nomeCargo;
-    }
-
-    public void setNomeCargo(String nomeCargo) {
-        this.nomeCargo = nomeCargo;
-    }
+@Schema(name = "Cargo")
+public record CargoDTO(
+        Integer idCargo,
+        @NotBlank(message = "campo obrigatorio")
+        @Size(min = 2, max = 100, message = "campo fora do tamanho padrao")
+        @Schema(name = "nome")
+        String nomeCargo
+        ) {
 }
