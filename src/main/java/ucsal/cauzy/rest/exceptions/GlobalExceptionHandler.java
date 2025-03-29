9eaa73ab7bc2f98d729eeeb86718a5200de084f0
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-29 10:25:15 -0300

    feat: add handlerMethodArgumentNotValidException on GlocalExcepitonhandler

--- src/main/java/ucsal/cauzy/rest/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ucsal/cauzy/rest/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,37 @@
 package ucsal.cauzy.rest.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ucsal.cauzy.domain.utils.exceptions.*;
+import ucsal.cauzy.rest.dto.ErroCampo;
 import ucsal.cauzy.rest.dto.ErroResposta;
 
+import java.util.List;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        log.error("Erro de validação: {} ", e.getMessage());
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        List<ErroCampo> listaErros = fieldErrors
+                .stream()
+                .map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação.",
+                listaErros);
+    }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
