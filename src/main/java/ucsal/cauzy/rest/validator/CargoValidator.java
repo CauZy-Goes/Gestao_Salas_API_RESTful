db4c9742a20e1ff4b4ea6435a345441ffa7f1f0c
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-29 17:34:12 -0300

    feat: create cargo validator

---
+++ src/main/java/ucsal/cauzy/rest/validator/CargoValidator.java
@@ -0,0 +1,30 @@
+package ucsal.cauzy.rest.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ucsal.cauzy.domain.entity.Cargo;
+import ucsal.cauzy.domain.repository.CargoRepository;
+import ucsal.cauzy.domain.repository.UsuarioRepository;
+import ucsal.cauzy.domain.utils.exception.NotFoundException;
+import ucsal.cauzy.domain.utils.exception.ResourceInUseException;
+
+@Component
+@RequiredArgsConstructor
+public class CargoValidator {
+
+    private final CargoRepository cargoRepository;
+
+    private final UsuarioRepository usuarioRepository;
+
+    public void existsCargo (Cargo cargo){
+        if(cargoRepository.findById(cargo.getIdCargo()).isEmpty()) {
+            throw new NotFoundException("Cargo não encontrado com o id " + cargo.getIdCargo());
+        }
+    }
+
+    public void validateDependencies (Cargo cargo) {
+        if (usuarioRepository.findByCargo(cargo).isEmpty()) {
+            throw new ResourceInUseException("Algum usuário depende desse cargo");
+        };
+    }
+}
