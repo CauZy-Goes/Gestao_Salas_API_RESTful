28c491da158f15ff490a746cb665b9bb3b247fc1
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-29 23:08:19 -0300

    feat: create status validator

--- src/main/java/ucsal/cauzy/domain/repository/SolicitacoesRepository.java
+++ src/main/java/ucsal/cauzy/domain/repository/SolicitacoesRepository.java
@@ -6,13 +6,17 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ucsal.cauzy.domain.entity.Solicitacoes;
+import ucsal.cauzy.domain.entity.Status;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SolicitacoesRepository extends JpaRepository<Solicitacoes, Integer> {
 
     // Buscar todas as solicitações feitas por um professor específico
      List<Solicitacoes> findByUsuarioSolicitante_IdUsuario(Integer idUsuarioSolicitante);
+
+     Optional<Solicitacoes> findByStatus(Status status);
 
     @Modifying
     @Query("DELETE FROM Solicitacoes s WHERE s.espacoFisico.idEspacoFisico = :idEspaco")
---
+++ src/main/java/ucsal/cauzy/rest/validator/StatusValidator.java
@@ -0,0 +1,33 @@
+package ucsal.cauzy.rest.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ucsal.cauzy.domain.entity.Status;
+import ucsal.cauzy.domain.repository.SolicitacoesRepository;
+import ucsal.cauzy.domain.repository.StatusRepository;
+import ucsal.cauzy.domain.utils.exception.NotFoundException;
+import ucsal.cauzy.domain.utils.exception.ResourceInUseException;
+
+@Component
+@RequiredArgsConstructor
+public class StatusValidator {
+
+    private final StatusRepository statusRepository;
+
+    private final SolicitacoesRepository solicitacoesRepository;
+
+    public void existsStatus(Integer id) {
+        if(!statusRepository.existsById(id)) {
+            throw new NotFoundException("Status não encontrado com o id : " + id);
+        }
+    }
+
+    public void validateDependencies(Integer id){
+        existsStatus(id);
+        Status status = statusRepository.findById(id).get();
+
+        if(solicitacoesRepository.findByStatus(status).isPresent()){
+            throw new ResourceInUseException("Alguma solicitacao depende desse status");
+        }
+    }
+}
