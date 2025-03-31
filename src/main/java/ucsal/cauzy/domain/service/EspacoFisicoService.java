5188f1d7ec3f64f7b39b619b170ee162d43f75c4
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-31 20:03:48 -0300

    feat: prepare to improve EspacoFisico Service

--- src/main/java/ucsal/cauzy/domain/service/EspacoFisicoService.java
+++ src/main/java/ucsal/cauzy/domain/service/EspacoFisicoService.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ucsal.cauzy.domain.entity.EspacoFisico;
 import ucsal.cauzy.domain.repository.EspacoFisicoRepository;
+import ucsal.cauzy.domain.repository.SolicitacoesRepository;
 import ucsal.cauzy.rest.validator.EspacoFisicoValidator;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,25 +17,34 @@ public class EspacoFisicoService {
 
     private final EspacoFisicoRepository espacoFisicoRepository;
 
+    private final SolicitacoesRepository solicitacoesRepository;
+
     public EspacoFisico findById(Integer id) {
         espacoFisicoValidator.existsEspacoFisico(id);
         return espacoFisicoRepository.findById(id).get();
     }
 
-    public void findAll (){
-
+    public List<EspacoFisico> findAll (){
+        return espacoFisicoRepository.findAll();
     }
 
-    public void save(EspacoFisico espacoFisico) {
-
+    public EspacoFisico save(EspacoFisico espacoFisico) {
+        espacoFisicoValidator.existsNumero(espacoFisico.getNumero());
+        return espacoFisicoRepository.save(espacoFisico);
     }
 
-
-    public void update(EspacoFisico espacoFisico) {
-
+    public void update(EspacoFisico espacoFisico, Integer id) {
+        espacoFisicoValidator.existsByIdAndNumero(id , espacoFisico.getNumero());
+        espacoFisico.setIdEspacoFisico(id);
+        espacoFisicoRepository.save(espacoFisico);
     }
 
-    public void delete(Long id) {
-
+    public void delete(Integer id) {
+        espacoFisicoValidator.existsEspacoFisico(id);
+        EspacoFisico espacoFisico = espacoFisicoRepository.findById(id).get();
+        if(!solicitacoesRepository.findByEspacoFisico(espacoFisico).isEmpty()){
+            solicitacoesRepository.deleteByEspacoFisicoId(id);
+        }
+        espacoFisicoRepository.delete(espacoFisico);
     }
 }
