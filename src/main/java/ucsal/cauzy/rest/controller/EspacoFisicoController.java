606517e8918af0d3c667c00c6219bdb479fd8a26
CauZy-Goes <cauafariasdev@gmail.com>
2025-03-31 20:18:14 -0300

    feat: improve findAll EndPoint in  EspacoFisico Controller

--- src/main/java/ucsal/cauzy/rest/controller/EspacoFisicoController.java
+++ src/main/java/ucsal/cauzy/rest/controller/EspacoFisicoController.java
@@ -1,5 +1,8 @@
 package ucsal.cauzy.rest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ucsal.cauzy.domain.service.EspacoFisicoService;
 import ucsal.cauzy.rest.dto.EspacoFisicoDTO;
+import ucsal.cauzy.rest.dto.EspacoFisicoPesquisaDTO;
+import ucsal.cauzy.rest.dto.StatusDTO;
 import ucsal.cauzy.rest.mapper.EspacoFisicoMapper;
 
 import java.util.List;
@@ -22,5 +27,19 @@ public class EspacoFisicoController implements GenericController{
     private final EspacoFisicoService espacoFisicoService;
 
     private final EspacoFisicoMapper espacoFisicoMapper;
+
+    @GetMapping
+    @Operation(summary = "Buscar todos", description = "Buscar todos os Espacos Fisicos")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Espacos Fisicos Listados com Sucesso")
+    })
+    public ResponseEntity<List<EspacoFisicoPesquisaDTO>> findAll() {
+        List<EspacoFisicoPesquisaDTO> listaEspacos = espacoFisicoService.findAll()
+                .stream()
+                .map(espacoFisicoMapper::toDTO)
+                .toList();
+
+        return ResponseEntity.ok(listaEspacos);
+    }
 }
 
