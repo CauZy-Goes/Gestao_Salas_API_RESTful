37f1c12734afc732562da56744b95b44bc934746
CauZy-Goes <cauafariasdev@gmail.com>
2025-04-06 08:34:51 -0300

    feat: create CustomClientAuthenticationToken

---
+++ src/main/java/ucsal/cauzy/security/configuration/CustomClientAuthenticationToken.java
@@ -0,0 +1,27 @@
+package ucsal.cauzy.security.configuration;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+public class CustomClientAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final String clientId;
+    private final String clientSecret;
+
+    public CustomClientAuthenticationToken(String clientId, String clientSecret) {
+        super(null);
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        setAuthenticated(false);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.clientSecret;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.clientId;
+    }
+}
+
